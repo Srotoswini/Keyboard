e2baa4023039ded5f0d832da0256d25b50eec27d
srotoswini <srotoswini.dev@gmail.com>
2022-05-05 23:47:26 +0530

    .

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const timestamps = [];
 
 timestamps.unshift(getTimestamp());
 
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomKey() {
+    return keys[getRandomNumber(0, keys.length-1)]
+  }
+
+  function targetRandomKey() {
+    const key = document.getElementById(getRandomKey());
+    key.classList.add("selected");
+    let start = Date.now()
+  }  
